20f09b32d2a54ff91c100a0307d43cb0302a0b30
rickypreacher-pixel <rickypreacher-pixel@users.noreply.github.com>
2026-05-08 16:25:58 -0700

    Feat: Prospects - live phone mask (###) ###-#### on phone field

--- src/App.tsx
+++ src/App.tsx
@@ -5756,7 +5756,7 @@ function ProspectsPage({prospects,setProspects,members}:any) {
           <Fld label="First Name *"><Inp value={form.first} onChange={sf("first")} placeholder="First"/></Fld>
           <Fld label="Last Name"><Inp value={form.last} onChange={sf("last")} placeholder="Last"/></Fld>
         </div>
-        <Fld label="Phone *"><Inp value={form.phone} onChange={sf("phone")} placeholder="(555) 000-0000"/></Fld>
+        <Fld label="Phone *"><Inp value={form.phone} onChange={v=>sf("phone")(fmtPhone(v))} placeholder="(555) 000-0000"/></Fld>
 
         <div style={{fontSize:11,color:MU,fontWeight:600,marginBottom:6,marginTop:4,textTransform:"uppercase",letterSpacing:0.4}}>Address</div>
         <Fld label="Street"><Inp value={form.street} onChange={sf("street")} placeholder="123 Main St"/></Fld>
